e688528c959e56b6261fe020f768e2812e0b6e16
KHAN687-AMIN <pubgcorporation894@gmail.com>
2024-09-02 15:12:09 +0500

    Update

---
+++ First-Jupyter-Notebook/main.py
@@ -0,0 +1,25 @@
+name:str = "Muhammad Amin Aamir"
+print(name)
+
+
+father_name:str = "Muhammad Aamir Khan"
+print(father_name)
+
+
+name:str = "Muhammad Amin Aamir"
+print( type(name) )
+print( id(name) ) 
+print( dir(name) )      
+
+
+name:str = "Muhammad Amin Aamir"
+
+#object.method()
+#object.attribute
+
+print( name.capitalize() ) 
+print( name.casefold() )
+print( name.center(50, "*") )
+print( name.lower() )
+print( name.upper() )
+print( name.title() )                   
